50e69db1fb3ce61d4d00c97b1a27504906740960
saulo <saulomcm@gmail.com>
2018-05-07 22:55:13 +0000

    Uodate ConsultaAdapter

--- AutoConsulta/app/src/main/java/com/android/luttos/autoconsulta/ConsultaAdapter.java
+++ AutoConsulta/app/src/main/java/com/android/luttos/autoconsulta/ConsultaAdapter.java
@@ -9,16 +9,17 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.luttos.autoconsulta.model.Consulta;
+
 import java.util.List;
 
 public class ConsultaAdapter extends BaseAdapter{
-    Context context;
-    List<Consulta> consultas;
+    private final List<Consulta> consultas;
+    private final Context context;
 
-
-    public ConsultaAdapter(Context context, List<Consulta>consultas){
-        this.consultas = consultas;
+    public ConsultaAdapter(Context context, List<Consulta> consultas) {
         this.context = context;
+        this.consultas = consultas;
     }
 
     @Override
@@ -33,39 +34,26 @@ public class ConsultaAdapter extends BaseAdapter{
 
     @Override
     public long getItemId(int position) {
-        return position;
+        return consultas.get(position).getId();
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         Consulta consulta = consultas.get(position);
 
-        ViewHolder holder = null;
+        LayoutInflater inflater = LayoutInflater.from(context);
 
-        if (convertView == null){
-            convertView = LayoutInflater.from(context).inflate(R.layout.item_consulta, null);
-            holder = new ViewHolder();
-
-            holder.image = (ImageView) convertView.findViewById(R.id.imageStatus);
-            holder.txtTitulo = (TextView) convertView.findViewById(R.id.txtTitulo);
-            holder.txtSubtitulo = (TextView) convertView.findViewById(R.id.txtSubTitulo);
-            holder.btAtualizar = (Button) convertView.findViewById(R.id.btAtualizar);
-
-            convertView.setTag(holder);
-        }else{
-            holder = (ViewHolder)convertView.getTag();
+        View view = convertView;
+        if (view == null) {
+            view = inflater.inflate(R.layout.item_consulta, parent, false);
         }
 
-        holder.txtSubtitulo.setText(consulta.getId().toString());
-        holder.txtTitulo.setText(consulta.getProcedimento());
+        TextView procedimento = view.findViewById(R.id.procedimento_consulta);
+        procedimento.setText(consulta.getProcedimento());
 
-        return convertView;
+        TextView cod_consulta = view.findViewById(R.id.codigo_consulta);
+        cod_consulta.setText(consulta.getCodigoConsulta());
+        return view;
     }
 
-    static class ViewHolder{
-        ImageView image;
-        TextView txtTitulo;
-        TextView txtSubtitulo;
-        Button btAtualizar;
-    }
 }
