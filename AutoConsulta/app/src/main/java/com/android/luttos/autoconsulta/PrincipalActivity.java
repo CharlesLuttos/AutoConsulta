c4acbbb1c31bffc575927f25500c1e1dee8318cb
Deivison Charles <charlesluttos@iCloud.com>
2018-05-10 17:59:57 -0300

    Revert "Método Apagar no contextMenu works!"
    This reverts commit bede8187c28a1a62dada2535a7e367f272abaf25.

--- AutoConsulta/app/src/main/java/com/android/luttos/autoconsulta/PrincipalActivity.java
+++ AutoConsulta/app/src/main/java/com/android/luttos/autoconsulta/PrincipalActivity.java
@@ -12,7 +12,6 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -105,6 +104,7 @@ public class PrincipalActivity extends AppCompatActivity {
     }
 
     @Override
+<<<<<<< HEAD
     public void onCreateContextMenu(ContextMenu menu, View v, final ContextMenu.ContextMenuInfo menuInfo) {
         MenuItem buscarMapa = menu.add("Buscar no Mapa");
         MenuItem criarAlerta = menu.add("Criar Alerta");
@@ -122,6 +122,13 @@ public class PrincipalActivity extends AppCompatActivity {
                 return false;
             }
         });
+=======
+    public void onCreateContextMenu(ContextMenu menu, View view, ContextMenu.ContextMenuInfo menuInfo){
+        if (view.getId() == R.id.ListaConsulta){
+            MenuInflater menuInflater = getMenuInflater();
+            menuInflater.inflate(R.menu.menu_opcoes, menu);
+        }
+>>>>>>> parent of bede818... Método Apagar no contextMenu works!
     }
 
     @Override
