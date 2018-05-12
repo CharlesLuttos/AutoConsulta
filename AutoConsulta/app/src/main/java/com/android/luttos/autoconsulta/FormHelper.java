35c8d2969d62fdd5c27254c51bfc6816d3aa9ee4
Deivison Charles <charlesluttos@iCloud.com>
2018-05-11 23:46:24 -0300

    Teste fix 3

--- AutoConsulta/app/src/main/java/com/android/luttos/autoconsulta/FormHelper.java
+++ AutoConsulta/app/src/main/java/com/android/luttos/autoconsulta/FormHelper.java
@@ -1,4 +1,49 @@
 package com.android.luttos.autoconsulta;
 
+import android.widget.EditText;
+import android.widget.RatingBar;
+
+import com.android.luttos.autoconsulta.model.Consulta;
+
+
 public class FormHelper {
+/*
+    private final EditText campoCodigo;
+    private final EditText campoData;
+    private final EditText campoPaciente;
+    private final EditText campoProcedimento;
+    private final EditText campoUnidadeAtendimento;
+    private final EditText campoLocal;
+    private final EditText campoSituacao;
+
+    private Consulta consulta;
+
+
+    public FormHelper(FormActivity activity){
+
+        campoCodigo =
+        campoNome = (EditText)activity.findViewById(R.id.form_nome);
+        campoTelefone = (EditText)activity.findViewById(R.id.form_telefone);
+        campoSite = (EditText)activity.findViewById(R.id.form_site);
+        campoNota = (RatingBar)activity.findViewById(R.id.form_nota);
+        consulta = new Consulta();
+    }
+
+    public Aluno pegaAluno() {
+        aluno.setNome(campoNome.getText().toString());
+        aluno.setTelefone(campoTelefone.getText().toString());
+        aluno.setSite(campoSite.getText().toString());
+        aluno.setNota(Double.valueOf(campoNota.getProgress()));
+        return aluno;
+    }
+
+    public void popularForm(Aluno aluno) {
+        campoNome.setText(aluno.getNome());
+        campoTelefone.setText(aluno.getTelefone());
+        campoSite.setText(aluno.getSite());
+        campoNota.setProgress(aluno.getNota().intValue());
+        this.aluno = aluno;
+
+    }
+*/
 }
